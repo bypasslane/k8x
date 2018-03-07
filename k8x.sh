39f2f6bc2c7472a8a488120b34c7e1c98f765728
Jon Ross <jross@bypassmobile.com>
2018-03-07 11:42:45 -0600

    walking deployments to yaml per namespace

--- k8x.sh
+++ k8x.sh
@@ -11,11 +11,18 @@ CONTEXT_NAME="$(kubectl config current-context)"
 #NAMESPACES=$(kubectl get namespace | awk -F " " '{print $1}' | tail -n+2)
 CONTEXT_DIR="$CONTEXT_NAME.$(date +%Y-%m-%d-%H%m.%S)"
 
-mkdir $CONTEXT_DIR
-cd $CONTEXT_DIR
+mkdir ${CONTEXT_DIR}
+cd ${CONTEXT_DIR}
 
-kubectl get namespace | awk -F " " '{print $1}' | tail -n+2 | while read line; do
-    mkdir $line
+kubectl get namespace | awk -F " " '{print $1}' | tail -n+2 | while read NAMESPACE; do
+    echo "Namespace: ${NAMESPACE}"
+    mkdir ${NAMESPACE}
+    cd ${NAMESPACE}
+    kubectl get deployment --namespace=${NAMESPACE} | awk -F " " '{print $1}' | tail -n+2 | while read DEPLOYMENT; do
+        echo -e "\t${NAMESPACE}.${DEPLOYMENT}"
+        kubectl get deployment ${DEPLOYMENT} --namespace=${NAMESPACE} -oyaml --export=true > ${DEPLOYMENT}.deployment.yaml
+    done
+    cd ..
 done
 
-echo $CONTEXT_DIR
+echo ${CONTEXT_DIR}
