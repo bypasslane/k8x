cc1fba495de54b651d5599e25dad69b09bb6768c
Jon Ross <jross@bypassmobile.com>
2018-03-07 16:00:59 -0600

    We can rebuild him. We have the technology. We can make him better than he was. Better, stronger, faster.

--- k8x.sh
+++ k8x.sh
@@ -4,11 +4,9 @@
 
 # mkdir for context
 # mksubDir for namespace
-# export all deployments, configmaps, services, injests
+# export all deployments, configmaps, services, SERVICE
 
 CONTEXT_NAME="$(kubectl config current-context)"
-
-#NAMESPACES=$(kubectl get namespace | awk -F " " '{print $1}' | tail -n+2)
 CONTEXT_DIR="$CONTEXT_NAME.$(date +%Y-%m-%d-%H%m.%S)"
 
 mkdir ${CONTEXT_DIR}
@@ -18,10 +16,38 @@ kubectl get namespace | awk -F " " '{print $1}' | tail -n+2 | while read NAMESPA
     echo "Namespace: ${NAMESPACE}"
     mkdir ${NAMESPACE}
     cd ${NAMESPACE}
+    echo -e "\t Deployments:"
     kubectl get deployment --namespace=${NAMESPACE} | awk -F " " '{print $1}' | tail -n+2 | while read DEPLOYMENT; do
-        echo -e "\t${NAMESPACE}.${DEPLOYMENT}"
-        kubectl get deployment ${DEPLOYMENT} --namespace=${NAMESPACE} -oyaml --export=true > ${DEPLOYMENT}.deployment.yaml
+        if ! [[ ${DEPLOYMENT} =~  "No resources found." ]]; then
+            echo -e "\t\t${DEPLOYMENT}.deployment.yaml"
+            kubectl get deployment ${DEPLOYMENT} --namespace=${NAMESPACE} -oyaml --export=true > ${DEPLOYMENT}.deployment.yaml &
+        fi
     done
+    wait
+    echo -e "\t Configmaps:"
+    kubectl get configmap --namespace=${NAMESPACE} | awk -F " " '{print $1}' | tail -n+2 | while read CONFIG_MAP; do
+        if ! [[ ${CONFIG_MAP} =~  "No resources found." ]]; then
+            echo -e "\t\t${CONFIG_MAP}.configmap.yaml"
+            kubectl get configmap ${CONFIG_MAP} --namespace=${NAMESPACE} -oyaml --export=true > ${CONFIG_MAP}.configmap.yaml &
+        fi
+    done
+    wait
+    echo -e "\t Services:"
+    kubectl get service --namespace=${NAMESPACE} | awk -F " " '{print $1}' | tail -n+2 | while read SERVICE; do
+        if ! [[ ${SERVICE} =~  "No resources found." ]]; then
+            echo -e "\t\t${SERVICE}.service.yaml"
+            kubectl get service ${SERVICE} --namespace=${NAMESPACE} -oyaml --export=true > ${SERVICE}.service.yaml &
+        fi
+    done
+    wait
+    echo -e "\t Ingresses:"
+    kubectl get ingress --namespace=${NAMESPACE} | awk -F " " '{print $1}' | tail -n+2 | while read INGRESS; do
+        if ! [[ ${INGRESS} =~  "No resources found." ]]; then
+            echo -e "\t\t${INGRESS}.ingress.yaml"
+            kubectl get ingress ${INGRESS} --namespace=${NAMESPACE} -oyaml --export=true > ${INGRESS}.ingress.yaml &
+        fi
+    done
+    wait
     cd ..
 done
 
